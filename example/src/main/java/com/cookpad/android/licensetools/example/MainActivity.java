c699b04f08891ee0afa820612c39c44e11b0af9a
iwataj <iwata@sansan.com>
2016-09-04 19:03:36 +0900

    Modify: Many changes to simplify example project for easy-to-understand
    * Add/Remove several dependencies for easy-to-understand
    * Add extra property to define application information
    * Add GoogleConversionTrackingSdk.jar
    * Add "Facebook Platform License.html" file
    * Update example/licenses.yml and assets/licenses.html
    * Remove unnecessary resources for sample code
    * Remove data binding and text view from main activity to simplify
    * Set minSdkVersion from 16 to 15

--- example/src/main/java/com/cookpad/android/licensetools/example/MainActivity.java
+++ example/src/main/java/com/cookpad/android/licensetools/example/MainActivity.java
@@ -1,24 +1,17 @@
 package com.cookpad.android.licensetools.example;
 
-import com.cookpad.android.licensetools.example.databinding.ActivityMainBinding;
-
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LICENSES_HTML_PATH = "file:///android_asset/licenses.html";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        WebView webView = binding.webView;
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.getSettings().setUseWideViewPort(true);
-        webView.loadUrl(LICENSES_HTML_PATH);
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        webView.loadUrl("file:///android_asset/licenses.html");
     }
 }
